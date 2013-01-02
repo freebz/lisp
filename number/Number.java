fa9fefcbc8de06d97efa06bdbfef240bb1e9dbb8
FreeBz <freebz@hananet.net>
2013-01-03 02:14:42 +0900

    add number by Java

---
+++ number/Number.java
@@ -0,0 +1,52 @@
+import java.io.*;
+
+public class Number {
+
+    private static int small = 1;
+    private static int big = 100;
+
+    private static int guess_my_number() {
+	return small + big >> 1;
+    }
+
+    private static int smaller() {
+	big = guess_my_number() - 1;
+	return guess_my_number();
+    }
+
+    private static int bigger() {
+	small = guess_my_number() + 1;
+	return guess_my_number();
+    }
+
+    private static int start_over() {
+	small = 1;
+	big = 100;
+	return guess_my_number();
+    }
+
+    public static void main (String args[]) throws IOException {
+
+	BufferedReader in =
+	    new BufferedReader(new InputStreamReader(System.in));
+	
+	while(true) {
+	    String inputString = in.readLine();     
+	    if("quit".equals(inputString)) {
+		break;
+	    }
+	    else if("guess-my-number".equals(inputString)) {
+		System.out.println(guess_my_number());
+	    }
+	    else if("bigger".equals(inputString)) {
+		System.out.println(bigger());
+	    }
+	    else if("smaller".equals(inputString)) {
+		System.out.println(smaller());
+	    }
+	    else if("start-over".equals(inputString)) {
+		System.out.println(start_over());
+	    }
+	}
+    }
+}
